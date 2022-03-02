ba7eddc55416f706a6b39ade3e3075681f299595
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-02 17:45:29 +0200

    slider info ready

--- src/components/index/Slider.tsx
+++ src/components/index/Slider.tsx
@@ -1,7 +1,28 @@
 import React, { FC } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+import "swiper/scss";
+
+const slides = [];
 
 const Slider: FC = () => {
-  return <div className="slider">Slider</div>;
+  return (
+    <div className="slider">
+      <Swiper
+        modules={[Pagination]}
+        navigation
+        pagination
+        spaceBetween={50}
+        slidesPerView={1}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
---
+++ src/constants/slides.tsx
@@ -0,0 +1,26 @@
+import cocktailImg1 from "../assets/images/slides/cocktail1.jpg";
+import cocktailImg2 from "../assets/images/slides/cocktail2.jpg";
+import cocktailImg3 from "../assets/images/slides/cocktail3.jpg";
+
+const slides = [
+  {
+    id: 1,
+    name: "Martini",
+    text: "The Martini is a cocktail that came onto the scene in multiple places at once, as bartenders began to experiment with gin and dry vermouth. Regardless, no origin story will leave you feeling as blissful and content as you will feel after drinking a classic, well-made Dry Martini.",
+    img: cocktailImg1,
+  },
+  {
+    id: 2,
+    name: "Martini2",
+    text: "The Martini is a cocktail that came onto the scene in multiple places at once, as bartenders began to experiment with gin and dry vermouth. Regardless, no origin story will leave you feeling as blissful and content as you will feel after drinking a classic, well-made Dry Martini.",
+    img: cocktailImg2,
+  },
+  {
+    id: 3,
+    name: "Martini3",
+    text: "The Martini is a cocktail that came onto the scene in multiple places at once, as bartenders began to experiment with gin and dry vermouth. Regardless, no origin story will leave you feeling as blissful and content as you will feel after drinking a classic, well-made Dry Martini.",
+    img: cocktailImg3,
+  },
+];
+
+export default slides;
