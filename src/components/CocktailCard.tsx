856dd8d4ee61d6655e68e054c1762dd42d1ce0b2
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-02 09:23:15 +0200

    CocktailCard

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,7 +1,38 @@
 import React, { FC } from "react";
 
-const CocktailCard: FC = () => {
-  return <div className="cocktail-card"> CocktailCard</div>;
+interface CocktailCardProps {
+  name: string;
+  isAlcoholic: boolean;
+  ingredientsArr: Array<string>;
+  image: string;
+}
+
+const CocktailCard: FC<CocktailCardProps> = ({
+  name,
+  isAlcoholic,
+  ingredientsArr,
+  image,
+}) => {
+  return (
+    <div className="cocktail-card">
+      {/* develop this */}
+      <header className="cocktail-card__header-container">
+        <div className="cocktail-card__header">
+          <p className="coctail-card__name">{name}</p>
+          <div className="cocktail-card__label"></div>
+        </div>
+      </header>
+      {/* end develop this */}
+      <div className="cocktail-card__ingredients">
+        {ingredientsArr.map((ingredient, index) => (
+          <div className="cocktail-card__ingredient" key={index}>
+            {ingredient}
+          </div>
+        ))}
+      </div>
+      <img src={image} alt={`cocktail-${name}`} />
+    </div>
+  );
 };
 
 export default CocktailCard;
