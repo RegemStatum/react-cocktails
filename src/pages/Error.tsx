99b6ef454b54aeee4e37abceda0e7fd362197cf2
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-08 14:38:52 +0200

    error page ready

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 
 const Error: FC = () => {
-  return <div>Error</div>;
+  return (
+    <div className="container page-min-height error-page">
+      <div className="error-page__inner-container">
+        <span className="error-page__error">Error 404</span>
+        <Link to="/" className="btn1 error-page__btn">
+          Back home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
