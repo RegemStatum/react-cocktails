66a6dcf4b7fbd2333b9bc64e2cd905a39062e622
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-08 14:44:46 +0200

    bugs fixed

--- src/pages/Cocktails.tsx
+++ src/pages/Cocktails.tsx
@@ -8,51 +8,51 @@ const Cocktails: FC = () => {
   const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
   const [cocktails, setCocktails] = useState<Array<cocktailCard>>([]);
   const [cocktailName, setCocktailName] = useState("");
-  const { favourites, checkIsCocktailLiked } = useAppContext();
-
-  const fetchCocktails = async (query: string) => {
-    try {
-      const response = await fetch(query);
-      let data = await response.json();
-      data = data.drinks.map((item: any) => {
-        const {
-          idDrink: id,
-          strDrink: name,
-          strDrinkThumb: image,
-          strAlcoholic,
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5,
-        } = item;
-
-        const isAlcoholic = strAlcoholic === "Alcoholic" ? true : false;
-        let ingredientsArr = [
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5,
-        ];
-        ingredientsArr = ingredientsArr.filter(
-          (ingredient) => ingredient !== null
-        );
-
-        const isLiked = checkIsCocktailLiked(id);
-
-        return { name, isAlcoholic, ingredientsArr, image, id, isLiked };
-      });
-
-      setCocktails(data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const { checkIsCocktailLiked } = useAppContext();
 
   useEffect(() => {
+    const fetchCocktails = async (query: string) => {
+      try {
+        const response = await fetch(query);
+        let data = await response.json();
+        data = data.drinks.map((item: any) => {
+          const {
+            idDrink: id,
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = item;
+
+          const isAlcoholic = strAlcoholic === "Alcoholic" ? true : false;
+          let ingredientsArr = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          ingredientsArr = ingredientsArr.filter(
+            (ingredient) => ingredient !== null
+          );
+
+          const isLiked = checkIsCocktailLiked(id);
+
+          return { name, isAlcoholic, ingredientsArr, image, id, isLiked };
+        });
+
+        setCocktails(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
     fetchCocktails(URL + cocktailName);
-  }, [cocktailName]);
+  }, [cocktailName, checkIsCocktailLiked]);
 
   const handleSearch = (e: React.SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
